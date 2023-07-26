576dd4553e84660b0359e4936555cc9b20ee381c
pedroxavier1451 <pedro.xavier.orellana@gmail.com>
2023-07-26 13:58:07 -0500

    Creación de componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CabeceraComponent } from './template/cabecera/cabecera.component';
+import { PieComponent } from './template/pie/pie.component';
+import { MenuComponent } from './template/menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CabeceraComponent,
+    PieComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,

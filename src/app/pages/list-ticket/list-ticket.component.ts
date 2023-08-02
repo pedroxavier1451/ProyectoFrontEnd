17c24f4abc05293122957adbaabc29c886f3dc4c
pedroxavier1451 <pedro.xavier.orellana@gmail.com>
2023-08-01 23:13:46 -0500

    cambios listarTicket

--- src/app/pages/list-ticket/list-ticket.component.ts
+++ src/app/pages/list-ticket/list-ticket.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { Ticket } from 'src/app/domain/ticket';
+import { TicketService } from 'src/app/services/ticket.service';
+
 
 @Component({
   selector: 'app-list-ticket',
@@ -7,11 +10,19 @@ import { Router } from '@angular/router';
   styleUrls: ['./list-ticket.component.scss']
 })
 export class ListTicketComponent {
+  listadoTicketsWS: any;
+  ticket: Ticket =new Ticket();
 
-  constructor(private router: Router)
-  {
+  constructor(private ticketService:TicketService,
+    private router: Router){
 
+      this.listadoTicketsWS=this.ticketService.getAll();
   }
+
+  ngOnInit():void{
+    this.listadoTicketsWS=this.ticketService.getAll();
+  }
+
 
   public generarFactura(){
     this.router.navigate(['paginas/factura'])
